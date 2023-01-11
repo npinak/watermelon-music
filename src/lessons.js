629ece23a638e36856137f36bce902d0e58ddc30
Pinak Nayak <77262741+npinak@users.noreply.github.com>
2023-01-11 15:58:30 -0800

    new laypout made

--- src/lessons.js
+++ src/lessons.js
@@ -14,63 +14,29 @@ function lessons(){
         main_page.removeChild(main_page.firstChild)
     }
 
-    // Create divs for lessons
-    const lessons_div = document.createElement('div');
-    lessons_div.classList.add("lesson_div")
-    lessons_div.setAttribute("id", "lesson_div")
+    // creating main center box where instrument lessons will be displayed
+    const center_box = document.createElement('div')
+    center_box.classList.add('center_box')
+    main_page.appendChild(center_box)
 
-    const guitar_div = document.createElement('div');
-    guitar_div.classList.add("music_lesson_div")
-    guitar_div.setAttribute("id", "guitar_div")
+    // adding instrument divs 
+    const guitar_div = document.createElement('div')
+    guitar_div.classList.add('guitar-image')
+    center_box.appendChild(guitar_div)
 
-    const bass_div = document.createElement('div');
-    bass_div.classList.add("music_lesson_div")
-    bass_div.setAttribute("id", "bass_div")
+    const drums_div = document.createElement('div')
+    drums_div.classList.add('drums-image')
+    center_box.appendChild(drums_div)
 
-    const piano_div = document.createElement('div');
-    piano_div.classList.add("music_lesson_div")
-    piano_div.setAttribute("id", "piano_div")
+    const bass_div = document.createElement('div')
+    bass_div.classList.add('bass-image')
+    center_box.appendChild(bass_div)
 
-    const drums_div = document.createElement('div');
-    drums_div.classList.add("music_lesson_div")
-    drums_div.setAttribute("id", "drums_div")
-    
+    const piano_div = document.createElement('div')
+    piano_div.classList.add('piano-image')
+    center_box.appendChild(piano_div)
 
-    // create pictures 
-    const guitar_img = document.createElement("img")
-    guitar_img.src = guitar 
-
-    const drums_img = document.createElement("img")
-    drums_img.src = drums
-    
-    const bass_img = document.createElement("img")
-    bass_img.src = bass
-    
-    const piano_img = document.createElement("img")
-    piano_img.src = piano 
-
-
-    // append elements
-    main_page.appendChild(lessons_div)
-    
-    lessons_div.appendChild(guitar_div)
-    guitar_div.appendChild(guitar_img)
-
-    lessons_div.appendChild(drums_div)
-    drums_div.appendChild(drums_img)
-
-    lessons_div.appendChild(bass_div)
-    bass_div.appendChild(bass_img)
-
-    lessons_div.appendChild(piano_div)
-    piano_div.appendChild(piano_img)
-    
-
-
-
-    // create text for pictures 
-
-
+   
 }
 
 export default lessons;
